53f94bb783481d0e96a383f83652544b54ea146c
Zach <dangerpine@gmail.com>
2022-10-21 15:56:49 -0500

    add noteflight links to /band page and simplify player html

---
+++ sort.js
@@ -0,0 +1,36 @@
+function sort(columnClassName, ascending) {
+    var tbody = document.getElementsByTagName("tbody")[0];
+    var rows = tbody.getElementsByTagName("tr");
+
+    var unsorted = true;
+
+    while (unsorted) {
+        unsorted = false
+
+        for (var r = 0; r < rows.length - 1; r++) {
+            var row = rows[r];
+            var nextRow = rows[r + 1];
+
+            var value = row.getElementsByClassName(columnClassName)[0].innerHTML;
+            var nextValue = nextRow.getElementsByClassName(columnClassName)[0].innerHTML;
+
+            value = value.replace(',', ''); // in case a comma is used in float number
+            nextValue = nextValue.replace(',', '');
+
+            if (!isNaN(value)) {
+                value = parseFloat(value);
+                nextValue = parseFloat(nextValue);
+            }
+
+            if (ascending ? value > nextValue : value < nextValue) {
+                tbody.insertBefore(nextRow, row);
+                unsorted = true;
+            }
+        }
+    }
+};
+
+window.onload = function () {
+    sort('song', true);
+    sort('instrumentation', false);
+}
